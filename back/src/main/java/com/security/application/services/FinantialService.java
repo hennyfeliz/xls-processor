62894f4e9c045e8baeeb4b1fff2fa411b80c2e64
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-07-27 01:18:05 -0400

    Capa de application

---
+++ back/src/main/java/com/security/application/services/FinantialService.java
@@ -0,0 +1,41 @@
+package com.security.application.services;
+
+
+import com.security.domain.model.Finantial;
+import com.security.domain.ports.in.FinantialUseCase;
+
+import java.util.List;
+import java.util.Optional;
+
+public class FinantialService implements FinantialUseCase {
+    public final FinantialUseCase finantialUseCase;
+
+    public FinantialService(FinantialUseCase finantialUseCase) {
+        this.finantialUseCase = finantialUseCase;
+    }
+
+    @Override
+    public Finantial createFinantial(Finantial finantial) {
+        return finantialUseCase.createFinantial(finantial);
+    }
+
+    @Override
+    public Optional<Finantial> getFinantialById(Long id) {
+        return finantialUseCase.getFinantialById(id);
+    }
+
+    @Override
+    public List<Finantial> getAllFinantials() {
+        return finantialUseCase.getAllFinantials();
+    }
+
+    @Override
+    public Optional<Finantial> updateFinantial(Long id, Finantial finantial) {
+        return finantialUseCase.updateFinantial(id, finantial);
+    }
+
+    @Override
+    public boolean deleteFinantial(Long id) {
+        return finantialUseCase.deleteFinantial(id);
+    }
+}
---
+++ back/src/main/java/com/security/application/usecases/FinantialUseCaseImpl.java
@@ -0,0 +1,43 @@
+package com.security.application.usecases;
+
+
+import com.security.domain.model.Finantial;
+import com.security.domain.ports.in.FinantialUseCase;
+import com.security.domain.ports.out.FinantialRepositoryPort;
+
+import java.util.List;
+import java.util.Optional;
+
+public class FinantialUseCaseImpl implements FinantialUseCase {
+    public final FinantialRepositoryPort finantialRepositoryPort;
+
+    public FinantialUseCaseImpl(FinantialRepositoryPort finantialRepositoryPort) {
+        this.finantialRepositoryPort = finantialRepositoryPort;
+    }
+
+    @Override
+    public Finantial createFinantial(Finantial finantial) {
+        return finantialRepositoryPort.save(finantial);
+    }
+
+    @Override
+    public Optional<Finantial> getFinantialById(Long id) {
+        return finantialRepositoryPort.findById(id);
+    }
+
+    @Override
+    public List<Finantial> getAllFinantials() {
+        return finantialRepositoryPort.findAll();
+    }
+
+    @Override
+    public Optional<Finantial> updateFinantial(Long id, Finantial finantial) {
+        return finantialRepositoryPort.update(finantial);
+    }
+
+    @Override
+    public boolean deleteFinantial(Long id) {
+        return finantialRepositoryPort.deleteById(id);
+    }
+}
+
